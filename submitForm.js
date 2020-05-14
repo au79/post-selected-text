43bda20e2344720cc3862ef32e28c3c715b07048
Chris Goldman <chris.goldman@akqa.com>
2020-05-14 12:37:47 -0700

    Timeout messages are working

--- submitForm.js
+++ submitForm.js
@@ -7,6 +7,7 @@ function submitForm(request, sender, sendResponse)
   const sendingRequestMessage = document.getElementById('sending-request')
   const successMessage = document.getElementById('success')
   const failureMessage = document.getElementById('failure')
+  const errorText = document.getElementById('error-text')
   const setUrlMessage = document.getElementById('set-url')
   const serverResponse = document.getElementById('server-response')
   const selectedText = document.getElementById('selected-text')
@@ -18,27 +19,35 @@ function submitForm(request, sender, sendResponse)
     sendingRequestMessage.style.display = 'inline'
   
     const xmlhttp = new XMLHttpRequest()   // new HttpRequest instance 
-    xmlhttp.open("POST", request.url)
-    xmlhttp.setRequestHeader("Content-Type", "application/json")
+
+    xmlhttp.onreadystatechange = function () {
+      console.log('STATE: ', xmlhttp.readyState)
+    }
 
     xmlhttp.onload = res => {
+      console.log('ONLOAD')
       sendingRequestMessage.style.display = 'none'
       successMessage.style.display = 'inline'
       serverResponse.innerHTML = res['target']['response']
     }
 
     xmlhttp.onerror = error => {
+      console.log('ONERROR')
       sendingRequestMessage.style.display = 'none'
       failureMessage.style.display = 'inline'
-      serverResponse.innerHTML = error.message
+      errorText.innerHTML = error.message
     }
 
-    xmlhttp.timeout = () => {
+    xmlhttp.timeout = 2000
+    xmlhttp.ontimeout = () => {
+      console.log('ONTIMEOUT')
       sendingRequestMessage.style.display = 'none'
       failureMessage.style.display = 'inline'
-      serverResponse.innerHTML = 'The request timed out!'
+      errorText.innerHTML = 'The request timed out!'
     }
 
+    xmlhttp.open("POST", request.url)
+    xmlhttp.setRequestHeader("Content-Type", "application/json")
 
     var postBodyObj = {}
     postBodyObj[request.key] = [request.value]
